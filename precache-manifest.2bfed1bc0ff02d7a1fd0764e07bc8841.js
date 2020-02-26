f69b34bd1bd48a54b09ce223380aa1e2efd20418
Luis Hernan Paez <lhernan.paez@gmail.com>
2020-02-26 19:43:26 -0300

    Updates

---
+++ precache-manifest.2bfed1bc0ff02d7a1fd0764e07bc8841.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9c22952644be629b316d6a6ed046a78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb05ce9825dc8fea9d98",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3149a0ef6aade5db0825",
+    "url": "/static/js/2.e90be324.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.e90be324.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb05ce9825dc8fea9d98",
+    "url": "/static/js/main.7c4e8bc0.chunk.js"
+  },
+  {
+    "revision": "0a93640c6fe0293b2b0c",
+    "url": "/static/js/runtime-main.24b56609.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
